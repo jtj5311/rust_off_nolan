5329d6d3620f43c48f5b806d59b8ab0dd49719d0
jtj5311 <jon@cardbubble.io>
2025-08-27 22:16:46 -0400

    python setup to measure like-like

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numba import njit
+import time 
 
 
 def setup(N:int)->np.ndarray:
@@ -18,15 +19,22 @@ def hot_loop(array: np.ndarray, steps: int)->float:
 
 def main(N:int, steps:int):
     array = setup(N)
+    X = hot_loop(array, steps)
 
-   
-    hot_loop(array, steps)
+
+    array = setup(N)
+    X = hot_loop(array, steps)
+
+    start = time.time()
+    X = hot_loop(array, steps)
+    print((time.time()-start)*1000, 'ms')
+    return X
 
 
 
 if __name__ == "__main__":
     N = 10**6
-    steps = 10**6
+    steps = 2*10**6
 
     main(N,steps)
 
