2776540bcbd1d43493ca1717015220854e68476c
jtj5311 <jon@cardbubble.io>
2025-08-27 22:06:24 -0400

    python setup

--- python/main.py
+++ python/main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello from python!")
+import numpy as np
+from numba import njit
+
+
+def setup(N:int)->np.ndarray:
+    array = np.random.rand(N)
+    return array
+
+@njit(cache = True)
+def hot_loop(array: np.ndarray, steps: int)->float:
+    output = 0
+    for i in range(steps):
+        output += array[i]
+
+    return output
+
+
+
+def main(N:int, steps:int):
+    array = setup(N)
+
+   
+    hot_loop(array, steps)
+
 
 
 if __name__ == "__main__":
-    main()
+    N = 10**6
+    steps = 10**6
+
+    main(N,steps)
+
+
+
+
+
